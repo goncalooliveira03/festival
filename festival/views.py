7d4d8326bd10c954374e7454fc2f8518d77144b7
goncalooliveira03 <46873718+goncalooliveira03@users.noreply.github.com>
2026-04-22 12:29:53 +0000

    Finalização Quizz

--- festival/views.py
+++ festival/views.py
@@ -8,23 +8,17 @@ def index_view(request):
 
 def dias_view(request):
     dias = Dia.objects.all()
-
     context = {'dias': dias}
-
     return render(request, 'festival/dias.html', context)
 
 
 def palcos_view(request):
     palcos = Palco.objects.all()
-
     context = {'palcos': palcos}
-
     return render(request, 'festival/palcos.html', context)
 
 
 def concerto_view(request, id):
     concerto = get_object_or_404(Concerto, id=id)
-
     context = {'concerto': concerto}
-
     return render(request, 'festival/concerto.html', context)
